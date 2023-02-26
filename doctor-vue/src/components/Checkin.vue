f31f0ab0511c265abf9e49f25f180f925ae46114
Robin Jain <robinjain97@gmail.com>
2023-02-26 15:40:15 +0100

    initial commit with setup
    Started project with a main component

---
+++ doctor-vue/src/components/Checkin.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <LvButton
+     :push="true"
+     :raised="true"
+     :deep-shadow="true"
+     :deep-shadow-hover="true"
+     :rounded="true"
+     label="Jeg er på jobb"
+     type="button"
+     size="xl"
+ />
+    </div>
+    <div>
+        <LvButton
+     :push="true"
+     :raised="true"
+     :deep-shadow="true"
+     :deep-shadow-hover="true"
+     :rounded="true"
+     label="Jeg drar fra jobb"
+     type="button"
+     size="xl"
+ />
+    </div>
+</template>
+
+<script lang="ts">
+import LvButton from 'lightvue/button';
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: "Checkin",
+    components: {LvButton},
+    setup() {
+        function checkIn() {
+            return;
+        }
+
+        function checkOut() {
+            return;
+        }
+        return {
+            checkIn,
+            checkOut
+        }
+    }
+})
+
+</script>
